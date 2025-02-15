8715229a8a3fd03acba826aeda01e92026b0810a
VictoriaGuimbard <victoria.guimbard@my.palmbeachstate.edu>
2025-02-14 21:13:10 -0500

    Update Octopus.cs

--- Octopus.cs
+++ Octopus.cs
@@ -1,62 +1,62 @@
 public class Octopus
 {
-  public readonly string Name;
-  public readonly int Legs = 8;
-  public int Age = 10;
+    public readonly string Name;
+    public readonly int Legs = 8;
+    public int Age = 10;
 
-  // Constructor to initialize the Name field
-  public Octopus(string name)
-  {
-    Name = name;
-  }
-
-  // Nested class within Octopus
-  public class Tentacle
-  {
-    public int Length { get; set; }
-    public void DisplayLength()
+    // Constructor to initialize the Name field
+    public Octopus(string name)
     {
-      Console.WriteLine($"Tentacle length is {Length} cm.");
+        Name = name;
     }
-  }
 
-  // Nested enum to describe octopus colors
-  public enum Color
-  {
-    Red,
-    Blue,
-    Green
-  }
+    // Nested class within Octopus
+    public class Tentacle
+    {
+        public int Length { get; set; }
+        public void DisplayLength()
+        {
+            Console.WriteLine($"Tentacle length is {Length} cm.");
+        }
+    }
 
-  // Method to access the nested enum
+    // Nested enum to describe octopus colors
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green
+    }
 
-public void DisaplyColor(Color color)
-{
-  Console.WriteLine($"The octopus color is {color}.");
+    // Method to access the nested enum
+    public void DisplayColor(Color color)
+    {
+        Console.WriteLine($"The octopus color is {color}.");
+    }
 }
 
 class Program
 {
-  static void Main(String[] args)
-  {
-    // Instantiate Octopus class
-    var o = new Octopus("Jack");
+    static void Main(string[] args)
+    {
+        // Instantiate the Octopus class
+        var o = new Octopus("Jack");
 
-    // Access and display octopus properties
-    Console.WriteLine($"The octopus's name is {o.Name}.");
-    Console.WriteLine($"The octopus's age is {o.Age} years old.");
-    Console.WriteLine($"The octopus has {o.Legs} legs.");
+        // Access and display octopus properties
+        Console.WriteLine($"The octopus's name is {o.Name}.");
+        Console.WriteLine($"The octopus's age is {o.Age} years.");
+        Console.WriteLine($"The octopus has {o.Legs} legs.");
 
-    // Create and use an istance of nested Tentacle class
-    var tentacle = new Octopus.Tentacle();
-    tentacle.Length = 50; // Set length of tentacles
-    tentacle.DisplayLength(); // Display the length of the tentacle
+        // Create and use an instance of the nested Tentacle class
+        var tentacle = new Octopus.Tentacle();
+        tentacle.Length = 50; // Set the length of the tentacle
+        tentacle.DisplayLength(); // Display the length of the tentacle
 
-    // Use nested Color enum
-    o.DisplayColor(Octopus.Color.Red);
+        // Use the nested Color enum
+        o.DisplayColor(Octopus.Color.Red);
 
-    // Modify and display Age
-    o.Age = 12;
-    Console.WriteLines($"The octopus's new age is {o.Age} years old.");
-  }
+        // Modify and display the Age
+        o.Age = 12;
+        Console.WriteLine($"The octopus's new age is {o.Age} years.");
+    }
 }
